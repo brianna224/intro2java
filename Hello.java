f530b327d13f60a04eb39c9eef6a8a08f000010c
Brianna Patrick <73494041+brianna224@users.noreply.github.com>
2021-01-26 14:58:56 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*******************************************
+* Java Program Name
+********************************************
+* Brianna Patrick
+* 01 26 21
+* 255 003
+*******************************************/
+
+public class Hello {
+   public static void main(String[] args) {
+   
+   System.out.println("<Brianna Patrick>");
+   System.out.println("<Project 01>");
+   System.out.println("<creating a file to output my name in a creative way>");
+   System.out.println("<01 26 21>");
+   System.out.println("<255 003>");
+   System.out.println();
+   }
+ }   
